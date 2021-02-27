075cf4671bb3a197149486124bcde3ce4f48a97d
Benjamin Dzandu <kojo.atieku@gmail.com>
2021-02-27 12:50:20 +0100

    GUI created

--- main.py
+++ main.py
@@ -20,10 +20,24 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 pix = PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=pix)
-canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.create_image(100, 112, image=pix)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+timer = Label(text="Timer", font=(FONT_NAME, 50), bg=YELLOW, fg=GREEN)
+timer.grid(column=1, row=0)
+
+start_btn = Button(text="Start")
+start_btn.grid(column=0, row=2)
+
+check_mark = Label(text="✔", bg=YELLOW, fg=GREEN)
+check_mark.grid(column=1, row=3)
+
+reset_btn = Button(text="Reset")
+reset_btn.grid(column=2, row=2)
 
 window.mainloop()
+
+
